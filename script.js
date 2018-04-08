e34b627ec2b639098247e88908eb00a979d09fb8
sgccodie6M <sgccodie6M@sgccodie6Ms-MacBook-Pro.local>
2018-04-08 15:10:27 +0800

    add the three starter files

---
+++ script.js
@@ -0,0 +1,37 @@
+// loads the code only after the entire thing has loaded
+document.addEventListener("DOMContentLoaded", () => {
+	// start your code here
+	console.log("version 1")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
